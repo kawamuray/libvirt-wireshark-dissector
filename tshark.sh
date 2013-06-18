b85237727d33eab29fc3a1d221a1891547a7a37a
Yuto KAWAMURA(kawamuray) <kawamuray.dadada@gmail.com>
2013-06-18 19:25:11 +0900

    tshark.sh: execute tshark in pdml capture mode, filtering only libvirt packets

---
+++ tshark.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Executing tshark as a non-root user requires you to belonging group 'wireshark'
+# and you need to run:
+# sudo chown root:wireshark local/bin/dumpcap
+# sudo setcap cap_net_raw,cap_net_admin=eip local/bin/dumpcap
+
+exec ./local/bin/tshark-mprivozn -i lo -R libvirt -O libvirt -T pdml
